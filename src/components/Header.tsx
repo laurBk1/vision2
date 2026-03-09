83d7a5c09701c53bd2ed1d62cf1d305e2375960f
laurBk1 <laur36600@gmail.com>
2026-03-09 03:18:53 +0200

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -103,7 +103,7 @@ const Header = () => {
             <img 
               src="/logo.webp" 
               alt="VisionEdit România Logo" 
-              className="h-10 md:h-12 w-auto"
+              className="h-14 md:h-16 w-auto"
               width="150"
               height="50"
               loading="eager"
